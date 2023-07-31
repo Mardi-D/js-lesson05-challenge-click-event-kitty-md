20599f90bc3aa7cb96708cf8dfcce833d696b019
mardi_d <mduckworth@gmail.com>
2023-07-31 14:17:28 -0700

    JS if and else exercise

--- js/script.js
+++ js/script.js
@@ -5,6 +5,20 @@ var cat = document.querySelector(".cat");
 //console.log(cat);
 
 button.addEventListener("click", function () {
-  //console.log("It's working!");
-  cat.classList.add("show");
+  if(cat.classList.contains("show")){
+  cat.classList.remove("show");
+  button.innerText = "Wait comeback!";
+  button.classList.add("disappear");
+    //console.log("Yes")
+  } else {cat.classList.add("show");
+  button.innerText = "Shoo, cat!!";
+  button.classList.remove("disappear");
+    //console.log("No");
+  }
 });
+
+
+
+
+//console.log("It's working!");
+//cat.classList.add("show");
